86a813f5b461fc8f18bab98fd9f96510ae0cb9a6
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-03 05:22:11 +0600

    mongodb connected with client server

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
 
-// middleware 
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+// connect to database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jufxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("proTechGear").collection("products");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
   res.send("Running Pro Tech Gear");
 });
 

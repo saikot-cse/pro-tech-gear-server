053eea66dfc05369a25d2e4e5615148e2a3671ff
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-07 04:12:44 +0600

    JWT for bonus section added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,21 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 // connect to database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jufxf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,6 +33,16 @@ async function run() {
   try {
     await client.connect();
     const productsCollection = client.db("proTechGear").collection("products");
+    // auth
+
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
     //create product api
     app.get("/product", async (req, res) => {
       const query = {};
@@ -24,6 +50,7 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     });
+    const blogsCollection = client.db("proTechGear").collection("blogs");
     //create blogs api
     app.get("/blogs", async (req, res) => {
       const query = {};
@@ -31,28 +58,43 @@ async function run() {
       const blogs = await cursor.toArray();
       res.send(blogs);
     });
+    const itemsCollection = client.db("proTechGear").collection("myitems");
+    //create myitems api
+    app.get('/myitems', verifyJWT, async (req, res) => {
+      const reqEmail = req.decoded.email;
+      const email = req.query.email;
+      if (email === reqEmail) {
+          const query = { email: email };
+          const cursor = productsCollection.find(query);
+          const myitems = await cursor.toArray();
+          res.send(myitems);
+      }
+      else {
+          res.status(403).send({ message: 'Forbidden access' })
+      }
+  })
 
-    app.get('/product/:id',async (req,res)=>{
+    app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const products = await productsCollection.findOne(query);
       res.send(products);
     });
 
     // add items
-    app.post("/product", async(req,res)=>{
+    app.post("/product", async (req, res) => {
       const newProduct = req.body;
       const result = await productsCollection.insertOne(newProduct);
       res.send(result);
     });
 
     //delete item
-    app.delete("/product/:id", async(req,res)=>{
+    app.delete("/product/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     //update item
     app.put("/product/:id", async (req, res) => {
@@ -63,21 +105,16 @@ async function run() {
       const updatedDoc = {
         $set: {
           name: newItem.name,
-          description: newItem.description,
+          desc: newItem.desc,
           price: newItem.price,
-          img: newItem.img,
+          image: newItem.image,
           supplierName: newItem.supplierName,
           quantity: newItem.quantity,
         },
       };
-      const result = await productsCollection.updateOne(
-        filter,
-        updatedDoc,
-        options
-      );
+      const result = await productsCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
     });
-
   } finally {
   }
 }

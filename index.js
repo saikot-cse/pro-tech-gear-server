03310c0023b2c7858d6a04772db5b46531f73176
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-05-08 04:00:04 +0600

    port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8008;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
